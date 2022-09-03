a64c3cfbdba37b7253a3b00274c74d3cc023c001
Mosiah Silva <100864562+mosiahrs@users.noreply.github.com>
2022-09-03 11:07:36 -0300

    Delete human.js
    remove funcao human para util renomeado para exibeEntrada()

--- js/human.js
+++
@@ -1,7 +0,0 @@
-
-function criarFalaHuman(entradaTexto) {
-    let criarFalaHuman = document.createElement("p")
-    saidaTexto.appendChild(criarFalaHuman)
-    criarFalaHuman.classList.add('textoEnviado')
-    criarFalaHuman.textContent = entradaTexto
-}
